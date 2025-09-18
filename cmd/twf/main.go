54e40f573d81c6def16289bbc5033d80b71dd31f
Minimal <jcm940308@gmail.com>
2025-09-18 21:46:59 +0900

    feat: 1단계 구현 완료

--- cmd/twf/main.go
+++ cmd/twf/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
 
 func main() {
-	fmt.Println("Hellow world")
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	fmt.Print("\033[2J\033[H")
+
+	fmt.Print("터미널 제어 테스트\r\n")
+	fmt.Print("아무 키나 누르면 종료됩니다...\r\n")
+
+	var b [1]byte
+	os.Stdin.Read(b[:])
+
+	fmt.Print("\033[2J\033[H")
 }
